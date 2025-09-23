88d4ad9099327adb17099556ea3c4a1ee8868367
Sidd-Cmd <siddvenkat83@gmail.com>
2025-09-23 08:29:51 +0530

    Create command_receiver.py

---
+++ src/onboard/communication/command_receiver.py
@@ -0,0 +1,13 @@
+"""
+Command Receiver: Receives commands from Ground Station
+"""
+import socket
+
+class CommandReceiver:
+    def __init__(self, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', port))
+
+    def receive(self):
+        data, addr = self.sock.recvfrom(4096)
+        return data, addr
